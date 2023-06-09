924fd38c0e82fab97dafbb66465c7a1f3771a671
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2023-06-09 10:35:49 -0300

    Add files via upload

---
+++ Fontes/Cap03/Cap03_Ex01/Cap03_Ex01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cap03_Ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int A, B, R;
+
+            Console.Write("Entre o valor <A>: ");
+            A = int.Parse(Console.ReadLine());
+            Console.Write("Entre o valor <B>: ");
+            B = int.Parse(Console.ReadLine());
+
+            R = A + B;
+
+            Console.WriteLine();
+            if (R > 10)
+                Console.WriteLine("Resultado = {0}", R);
+
+            Console.WriteLine();
+            Console.Write("Tecle <Enter> para encerrar... ");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Fontes/Cap03/Cap03_Ex02/Cap03_Ex02/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cap03_Ex02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int A, B, R;
+
+            Console.Write("Entre o valor <A>: ");
+            A = int.Parse(Console.ReadLine());
+            Console.Write("Entre o valor <B>: ");
+            B = int.Parse(Console.ReadLine());
+
+            R = A + B;
+
+            Console.WriteLine();
+            if (R > 10)
+                Console.WriteLine("Resultado ultrapassa dez.");
+            else
+                Console.WriteLine("Resultado nao ultrapassa dez.");
+
+            Console.WriteLine();
+            Console.Write("Tecle <Enter> para encerrar... ");
+            Console.ReadLine();
+
+        }
+    }
+}
