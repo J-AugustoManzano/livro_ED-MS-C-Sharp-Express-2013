33b3d8523beca6e687b20ad22b076137360f78cc
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2023-06-09 11:03:44 -0300

    Add files via upload

---
+++ Fontes/Cap08/Cap08_Ex02/Cap08_Ex02/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cap08_Ex02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label3.Text = textBox1.Text + " " + textBox2.Text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            label3.Text = "";
+            textBox1.Focus();
+        }
+    }
+}
---
+++ Fontes/Cap08/Cap08_Ex03/Cap08_Ex03/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cap08_Ex03
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int R, X, A, B;
+            A = int.Parse(textBox1.Text);
+            B = int.Parse(textBox2.Text);
+            X = A + B;
+            if (X >= 10)
+            {
+                R = X + 5;
+                MessageBox.Show("R = " + R.ToString(), "Resultado V");
+            }
+            else
+            {
+                R = X - 7;
+                MessageBox.Show("R = " + R.ToString(), "Resultado F");
+            }
+            textBox1.Focus();
+        }
+    }
+}
