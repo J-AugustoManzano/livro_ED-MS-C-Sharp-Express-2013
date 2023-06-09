38c918f97d6de9e7e1f26cd53bdb7eff8fcbbdb6
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2023-06-09 10:55:05 -0300

    Add files via upload

---
+++ Fontes/Cap04/Cap04_Ex08/Cap04_Ex08/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cap04_Ex08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int VALOR = 1, RESULTADO;
+
+            RESULTADO = VALOR++;
+            Console.WriteLine("RESULTADO = {0}", RESULTADO);
+            Console.WriteLine("VALOR     = {0}", VALOR);
+            Console.WriteLine();
+
+            Console.Write("Tecle algo para encerrar... ");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Fontes/Cap04/Cap04_Ex10/Cap04_Ex10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cap04_Ex10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int I = 1; I <= 10; I += 2)
+                Console.WriteLine("I = {0}", I);
+            Console.WriteLine();
+
+            Console.Write("Tecle algo para encerrar... ");
+            Console.ReadKey();
+
+        }
+    }
+}
