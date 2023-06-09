2c9c5eb0f16a27da01d03588157640cd88b66ecd
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2023-06-09 11:01:28 -0300

    Add files via upload

---
+++ Fontes/Cap06/Cap06_Ex05/Cap06_Ex05/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cap06_Ex05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[,] A = new int[3, 3];
+
+            int I, J, LIN = 7, COL = 0;
+
+            Console.SetCursorPosition(0, 0);
+            Console.Write("Entre um valor na coordenada:");
+            Console.SetCursorPosition(0, 2);
+            Console.Write("Linha ...: ");
+            Console.SetCursorPosition(0, 3);
+            Console.Write("Coluna ..: ");
+
+            for (I = 0; I <= 2; I++)
+            {
+                for (J = 0; J <= 2; J++)
+                {
+                    Console.SetCursorPosition(11, 2);
+                    Console.Write("{0}", I + 1);
+                    Console.SetCursorPosition(11, 3);
+                    Console.Write("{0}", J + 1);
+                    Console.SetCursorPosition(0, 5);
+                    Console.Write("--> ");
+                    A[I, J] = int.Parse(Console.ReadLine());
+                    Console.SetCursorPosition(4, 5);
+                    Console.Write("    ");
+                    Console.SetCursorPosition(COL, LIN);
+                    Console.Write("{0,4}", A[I, J]);
+                    COL += 6;
+                }
+                LIN += 2;
+                COL -= 18;
+            }
+
+            Console.SetCursorPosition(0, 13);
+            Console.Write("Saida - Diagonal Principal");
+            Console.SetCursorPosition(0, 15);
+
+            for (I = 0; I <= 2; I++)
+            {
+                for (J = 0; J <= 2; J++)
+                {
+                    if (I == J)
+                    {
+                        Console.Write("A[{0},{1}]", I + 1, J + 1);
+                        Console.WriteLine(" = {0,4}", A[I, J]);
+                    }
+                }
+            }
+
+            Console.WriteLine();
+            Console.Write("Tecle algo para encerrar... ");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Fontes/Cap06/Cap06_Ex06/Cap06_Ex06/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cap06_Ex06
+{
+    class Program
+    {
+
+        public struct CAD_ALUNO
+        {
+            public string NOME;
+            public int SALA;
+            public float MEDIA;
+        };
+
+        static void Main(string[] args)
+        {
+
+            CAD_ALUNO[] ALUNO = new CAD_ALUNO[8];
+            CAD_ALUNO X;
+            int I, J, LIN;
+
+            // Rotina para a entrada dos nomes e notas
+
+            for (I = 0; I <= 7; I++)
+            {
+                Console.Clear();
+                Console.WriteLine("Cadastro de Alunos");
+                Console.WriteLine();
+                Console.Write("Entre o {0}o. nome ...: ", I + 1);
+                ALUNO[I].NOME = Console.ReadLine();
+                Console.Write("Entre a sala .......: ");
+                ALUNO[I].SALA = int.Parse(Console.ReadLine());
+                Console.Write("Entre a media ......: ");
+                ALUNO[I].MEDIA = float.Parse(Console.ReadLine());
+                Console.WriteLine();
+            }
+
+            // Classificacao dos dados
+
+            for (I = 0; I <= 6; I++)
+                for (J = I + 1; J <= 7; J++)
+                    if (ALUNO[I].NOME.CompareTo(ALUNO[J].NOME) > 0)
+                    {
+                        X = ALUNO[I];
+                        ALUNO[I] = ALUNO[J];
+                        ALUNO[J] = X;
+                    }
+
+            // Apresentacao da tabela nome/notas/media
+
+            Console.Clear();
+            Console.WriteLine("Relatorio Geral");
+            Console.WriteLine();
+
+            Console.SetCursorPosition(0, 3);
+            Console.Write("Aluno");
+            Console.SetCursorPosition(41, 3);
+            Console.Write("Sala");
+            Console.SetCursorPosition(48, 3);
+            Console.WriteLine("Media");
+
+            LIN = 6;
+            for (I = 0; I <= 7; I++)
+            {
+                Console.SetCursorPosition(0, LIN);
+                Console.Write(ALUNO[I].NOME.ToUpper());
+                Console.SetCursorPosition(41, LIN);
+                Console.Write("{0,4}", ALUNO[I].SALA);
+                Console.SetCursorPosition(48, LIN);
+                Console.Write("{0,5:0.00}", ALUNO[I].MEDIA);
+                LIN++;
+            }
+
+            Console.SetCursorPosition(0, LIN);
+            Console.WriteLine();
+            Console.Write("Tecle algo para encerrar... ");
+            Console.ReadKey();
+
+        }
+    }
+}
