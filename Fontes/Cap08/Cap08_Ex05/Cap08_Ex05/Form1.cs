8164a2c01ac40427c762e4bfb06ae0834159e02e
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2023-06-09 11:05:14 -0300

    Add files via upload

---
+++ Fontes/Cap08/Cap08_Ex05/Cap08_Ex05/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cap08_Ex05
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult RESP;
+            RESP = MessageBox.Show
+            (
+                "Você é honesto?",
+                "Uma Pergunta",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question
+            );
+            if (RESP == DialogResult.Yes)
+                MessageBox.Show
+                (
+                    "Parabéns!!!",
+                    "Respondeu SIM",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information
+                );
+            else
+                MessageBox.Show
+                (
+                    "Seu pilantra!!!",
+                    "Respondeu NÃO",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                );
+        }
+    }
+}
---
+++ Fontes/Cap08/Cap08_Ex06/Cap08_Ex06/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cap08_Ex06
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float R = 0, A, B;
+            A = float.Parse(textBox1.Text);
+            B = float.Parse(textBox2.Text);
+            if (radioButton1.Checked)
+                R = A + B;
+            if (radioButton2.Checked)
+                R = A - B;
+            if (radioButton3.Checked)
+                R = A * B;
+            if (radioButton4.Checked)
+                if (B == 0)
+                    MessageBox.Show("ERRO – Divisão zero!");
+                else
+                    R = A / B;
+            textBox3.Text = R.ToString();
+            textBox1.Focus();
+        }
+    }
+}
